59ec64c08a165f321bea5fc8b539ac1a0d26c8bf
cshekar2k10 <cshekar2k10@gmail.com>
2023-04-10 19:39:26 +0530

    Updating roboshop-infra

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "network" {
   source = "github.com/chandrashekarsubuddi/tf-module-vpc"
   env = var.env
+
   for_each = var.vpc
   cidr_block = each.value.cidr_block
   subnets_cidr = each.value.subnets_cidr
